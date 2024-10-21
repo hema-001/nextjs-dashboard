3bf6e97cf51370f79a9c746a80cc567a52a78d52
hema-001 <hema-001@hotmail.com>
2024-10-21 15:48:03 +0800

    fixed broken navigation issue, login route was in a wrong location

--- auth.ts
+++ auth.ts
@@ -16,25 +16,27 @@ async function getUser(email: string): Promise<User | undefined> {
     }
   }
   
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  providers: [Credentials({
-    async authorize(credentials){
-        const parsedCredentials = z
-        .object({email: z.string().email(), password: z.string().min(6)})
-        .safeParse(credentials);
-
-        if(parsedCredentials.success){
-            const {email, password} = parsedCredentials.data;
+  export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+      Credentials({
+        async authorize(credentials) {
+          const parsedCredentials = z
+            .object({ email: z.string().email(), password: z.string().min(6) })
+            .safeParse(credentials);
+   
+          if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
             const user = await getUser(email);
-            if(!user) return null;
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if(passwordMatch) return user;
-        }
-
-        console.log('invalid credentials');
-        return null;
-    },
-  },
-)],
-});
+            if (!user) return null;
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+            if (passwordsMatch) return user;
+          }
+   
+          console.log('Invalid credentials');
+          return null;
+        },
+      }),
+    ],
+  });

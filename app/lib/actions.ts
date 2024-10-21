0396111c85ee5d44e27ee67896af041fb487ff25
hema-001 <hema-001@hotmail.com>
2024-10-21 15:51:25 +0800

    removed blocking throw statement in deleteInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -126,7 +126,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string){
-    throw new Error("Failed to delete invoice")
     try{
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         return { message: 'Deleted Invoice.' };
